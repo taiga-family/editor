87ff5d35d32ddad85b7bbea1c0d3d48d50cc312a
splincode <omaxphp@yandex.ru>
2023-07-05 11:46:37 +0000

    Deploying to gh-pages from @ Tinkoff/tui-editor@6daf751ae94a52bb020f2bc4a14e823843d78eae 🚀

---
+++ 1114.54d9ba687b1a0aa661db.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktui_editor=self.webpackChunktui_editor||[]).push([[1114],{1114:t=>{t.exports='<tui-editor\n    class="editor"\n    [formControl]="control"\n    [tools]="builtInTools"\n></tui-editor>\n\n<h4>HTML:</h4>\n<tui-editor-socket [content]="control.value || \'\'"></tui-editor-socket>\n\n<h4>Text:</h4>\n<p>{{ control.value }}</p>\n'}}]);
