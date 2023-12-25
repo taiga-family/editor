3d64ee8c1e4aac9c188df20af279e0296c2381bc
splincode <omaxphp@yandex.ru>
2023-12-25 10:55:17 +0300

    chore: refactor

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,211 +1,155 @@
-/* eslint-disable @typescript-eslint/quotes */
 import type {Routes} from '@angular/router';
+import {tuiProvideRoutePageTab as route} from '@taiga-ui/addon-doc';
 
 import {TuiDemoPath} from './constants/demo-path';
 
 export const routes: Routes = [
-    {
+    route({
         path: TuiDemoPath.StarterKit,
         loadComponent: async () => import('./pages/starter/editor-starter.component'),
-        children: [
-            {
-                path: ':tab',
-                loadComponent: async () =>
-                    import('./pages/starter/editor-starter.component'),
-            },
-        ],
-        data: {
-            title: 'StarterKit',
-        },
-    },
-    {
+        title: 'StarterKit',
+    }),
+    route({
         path: 'stackblitz',
         loadComponent: async () =>
             import('./pages/stackblitz/starter/stackblitz-starter.component'),
-        data: {
-            title: 'Stackblitz Starter',
-        },
-    },
-    {
+        title: 'Stackblitz Starter',
+    }),
+    route({
         path: TuiDemoPath.ColorPicker,
         loadComponent: async () =>
             import('./pages/color-picker/editor-input-color-picker.component'),
-        children: [
-            {
-                path: ':tab',
-                loadComponent: async () =>
-                    import('./pages/color-picker/editor-input-color-picker.component'),
-            },
-        ],
-        data: {
-            title: 'Input color picker',
-        },
-    },
-    {
+        title: 'Input color picker',
+    }),
+    route({
         path: TuiDemoPath.AngularContenteditableAccessor,
         loadComponent: async () =>
             import(
                 './pages/angular-contenteditable-accessor/angular-contenteditable-accessor.component'
             ),
-        data: {
-            title: 'Contenteditable accessor',
-        },
-    },
-    {
+        title: 'Contenteditable accessor',
+    }),
+    route({
         path: TuiDemoPath.Font,
         loadComponent: async () => import('./pages/font/editor-font.component'),
-        data: {
-            title: 'Font',
-        },
-    },
-    {
+        title: 'Font',
+    }),
+    route({
         path: TuiDemoPath.Anchors,
         loadComponent: async () => import('./pages/anchors/editor-anchors.component'),
-        data: {
-            title: 'Anchors',
-        },
-    },
-    {
+        title: 'Anchors',
+    }),
+    route({
         path: TuiDemoPath.Focus,
         loadComponent: async () => import('./pages/focus/editor-focus.component'),
-        data: {
-            title: 'Focus',
-        },
-    },
-    {
+        title: 'Focus',
+    }),
+    route({
         path: TuiDemoPath.Groups,
         loadComponent: async () => import('./pages/groups/editor-groups.component'),
-        data: {
-            title: 'Groups',
-        },
-    },
-    {
+        title: 'Groups',
+    }),
+    route({
         path: TuiDemoPath.UploadFiles,
         loadComponent: async () =>
             import('./pages/upload-files/editor-upload-files.component'),
-        data: {
-            title: 'Upload files',
-        },
-    },
-    {
+        title: 'Upload files',
+    }),
+    route({
         path: TuiDemoPath.PasteEmojiTool,
         loadComponent: async () =>
             import('./pages/custom-tool/paste-emoji/editor-paste-emoji.component'),
-        data: {
-            title: 'Editor — Paste emoji custom tool',
-        },
-    },
-    {
+        title: 'Editor — Paste emoji custom tool',
+    }),
+    route({
         path: TuiDemoPath.ColorPickerTool,
         loadComponent: async () =>
             import('./pages/custom-tool/color-picker/editor-color-picker.component'),
-        data: {
-            title: 'Editor — Color picker custom tool',
-        },
-    },
-    {
+        title: 'Editor — Color picker custom tool',
+    }),
+    route({
         path: TuiDemoPath.PasteImageTool,
         loadComponent: async () =>
             import('./pages/custom-tool/paste-image/editor-paste-image-tool.component'),
-        data: {
-            title: 'Editor — Paste image custom tool',
-        },
-    },
-    {
+        title: 'Editor — Paste image custom tool',
+    }),
+    route({
+        path: TuiDemoPath.PasteImageTool,
+        loadComponent: async () =>
+            import('./pages/custom-tool/paste-image/editor-paste-image-tool.component'),
+        title: 'Editor — Paste image custom tool',
+    }),
+    route({
         path: TuiDemoPath.HighlightText,
         loadComponent: async () =>
             import('./pages/highlight/text/editor-mark-text.component'),
-        data: {
-            title: 'Editor — Highlight text',
-        },
-    },
-    {
+        title: 'Editor — Highlight text',
+    }),
+    route({
         path: TuiDemoPath.ProcessingLegacyHtml,
         loadComponent: async () =>
             import('./pages/processing/legacy-html/editor-legacy-html.component'),
-        data: {
-            title: 'Editor — Legacy html',
-        },
-    },
-    {
+        title: 'Editor — Legacy html',
+    }),
+    route({
         path: TuiDemoPath.ProcessingCleanupHtml,
         loadComponent: async () =>
             import('./pages/processing/cleanup-html/editor-cleanup-html.component'),
-        data: {
-            title: 'Editor — Cleanup html',
-        },
-    },
-    {
+        title: 'Editor — Cleanup html',
+    }),
+    route({
         path: TuiDemoPath.ProcessingMarkdown,
         loadComponent: async () =>
             import('./pages/processing/markdown/editor-markdown.component'),
-        data: {
-            title: 'Editor — Markdown',
-        },
-    },
-    {
+        title: 'Editor — Markdown',
+    }),
+    route({
         path: TuiDemoPath.HighlightCode,
         loadComponent: async () =>
             import('./pages/highlight/code/editor-code-block.component'),
-        data: {
-            title: 'Editor — Highlight code',
-        },
-    },
-    {
+        title: 'Editor — Highlight code',
+    }),
+    route({
         path: TuiDemoPath.ImagesResizable,
         loadComponent: async () =>
             import('./pages/images/resizable/editor-resizable-images.component'),
-        data: {
-            title: 'Editor  — Resizable images',
-        },
-    },
-    {
+        title: 'Editor  — Resizable images',
+    }),
+    route({
         path: TuiDemoPath.ImagesPreview,
         loadComponent: async () =>
             import('./pages/images/preview/editor-preview-images.component'),
-        data: {
-            title: 'Editor  — Preview images',
-        },
-    },
-    {
+        title: 'Editor  — Preview images',
+    }),
+    route({
         path: TuiDemoPath.ImagesUpload,
         loadComponent: async () =>
             import('./pages/images/upload/editor-upload-images.component'),
-        data: {
-            title: 'Editor — Upload images',
-        },
-    },
-    {
+        title: 'Editor — Upload images',
+    }),
+    route({
         path: TuiDemoPath.EmbedYoutube,
         loadComponent: async () =>
             import('./pages/embed/youtube/editor-embed-youtube.component'),
-        data: {
-            title: 'Editor — YouTube',
-        },
-    },
-    {
+        title: 'Editor — YouTube',
+    }),
+    route({
         path: TuiDemoPath.EmbedIframe,
         loadComponent: async () =>
             import('./pages/embed/iframe/editor-embed-iframe.component'),
-        data: {
-            title: 'Editor — Iframe',
-        },
-    },
-    {
+        title: 'Editor — Iframe',
+    }),
+    route({
         path: TuiDemoPath.EmbedHtml5,
         loadComponent: async () =>
             import('./pages/embed/html5/editor-embed-html5.component'),
-        data: {
-            title: 'Editor — HTML5',
-        },
-    },
-    {
+        title: 'Editor — HTML5',
+    }),
+    route({
         path: TuiDemoPath.Changelog,
         loadComponent: async () => import('./pages/changelog/editor-changelog.component'),
-        data: {
-            title: 'Changelog',
-        },
-    },
+        title: 'Changelog',
+    }),
     {
         path: '**',
         redirectTo: TuiDemoPath.StarterKit,
