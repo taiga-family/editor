b2ac8434e6b5f050be3b9e7f40fe17356f69fe63
splincode <omaxphp@yandex.ru>
2024-02-26 10:49:21 +0300

    chore: update submodule

--- projects/demo/src/app/pages/stackblitz/stackblitz-deps.service.ts
+++ projects/demo/src/app/pages/stackblitz/stackblitz-deps.service.ts
@@ -1,44 +1,39 @@
 import {Injectable} from '@angular/core';
-import {TUI_VERSION} from '@taiga-ui/cdk';
+// import {TUI_VERSION} from '@taiga-ui/cdk';
 
 @Injectable({providedIn: 'root'})
 export class StackblitzDepsService {
     async get(): Promise<Record<string, string>> {
-        const {dependencies: demoDeps} = await import('@taiga-ui/submodule-demo-package');
-        const {dependencies: cdkDeps} = await import('@taiga-ui/submodule-cdk-package');
-        const {dependencies, devDependencies} = await import(
-            '@taiga-ui/submodule-package'
-        );
+        // const {dependencies: demoDeps} = await import('@taiga-ui/submodule-demo-package');
+        // const {dependencies: cdkDeps} = await import('@taiga-ui/submodule-cdk-package');
+        const {devDependencies} = await import('@taiga-ui/submodule-package');
 
         return {
-            '@angular/cdk': dependencies['@angular/cdk'],
-            '@angular/core': dependencies['@angular/core'],
-            '@angular/common': dependencies['@angular/common'],
-            '@angular/compiler': dependencies['@angular/compiler'],
-            '@angular/forms': dependencies['@angular/forms'],
-            '@angular/localize': dependencies['@angular/localize'],
-            '@angular/platform-browser': dependencies['@angular/platform-browser'],
-            '@angular/platform-browser-dynamic':
-                dependencies['@angular/platform-browser-dynamic'],
-            '@angular/animations': dependencies['@angular/animations'],
-            '@angular/router': dependencies['@angular/router'],
-            '@taiga-ui/cdk': TUI_VERSION,
-            '@taiga-ui/i18n': TUI_VERSION,
-            '@taiga-ui/core': TUI_VERSION,
-            '@taiga-ui/kit': TUI_VERSION,
-            '@taiga-ui/icons': TUI_VERSION,
-            '@taiga-ui/styles': TUI_VERSION,
-            '@ng-web-apis/common': cdkDeps['@ng-web-apis/common'],
-            '@ng-web-apis/resize-observer': cdkDeps['@ng-web-apis/resize-observer'],
-            '@ng-web-apis/mutation-observer': cdkDeps['@ng-web-apis/mutation-observer'],
-            'zone.js': dependencies['zone.js'],
-            dompurify: demoDeps.dompurify,
+            '@angular/cdk': devDependencies['@angular/cdk'],
+            '@angular/compiler': devDependencies['@angular/cdk'],
+            '@angular/core': devDependencies['@angular/core'],
+            '@angular/common': devDependencies['@angular/common'],
+            '@angular/forms': devDependencies['@angular/forms'],
+            '@angular/platform-browser': devDependencies['@angular/platform-browser'],
+            '@angular/animations': devDependencies['@angular/animations'],
+            '@angular/router': devDependencies['@angular/router'],
+            '@taiga-ui/cdk': 'latest',
+            '@taiga-ui/i18n': 'latest',
+            '@taiga-ui/core': 'latest',
+            '@taiga-ui/kit': 'latest',
+            '@taiga-ui/icons': 'latest',
+            '@taiga-ui/styles': 'latest',
+            '@ng-web-apis/common': 'latest',
+            '@ng-web-apis/resize-observer': 'latest',
+            '@ng-web-apis/mutation-observer': 'latest',
+            // 'zone.js': demoDeps['zone.js'],
+            // dompurify: demoDeps.dompurify,
             typescript: devDependencies.typescript,
-            rxjs: dependencies.rxjs,
+            // rxjs: dependencies.rxjs,
             '@tinkoff/tui-editor': '1.x.x',
-            '@tinkoff/ng-polymorpheus': cdkDeps['@tinkoff/ng-polymorpheus'],
-            '@tinkoff/ng-dompurify': demoDeps['@tinkoff/ng-dompurify'],
-            '@tinkoff/ng-event-plugins': cdkDeps['@tinkoff/ng-event-plugins'],
+            // '@tinkoff/ng-polymorpheus': cdkDeps['@tinkoff/ng-polymorpheus'],
+            // '@tinkoff/ng-dompurify': demoDeps['@tinkoff/ng-dompurify'],
+            // '@tinkoff/ng-event-plugins': cdkDeps['@tinkoff/ng-event-plugins'],
         };
     }
 }
