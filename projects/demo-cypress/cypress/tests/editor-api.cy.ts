36b9bdec1cb29659cd78aaed4b7cef1edd91188c
splincode <omaxphp@yandex.ru>
2024-02-28 12:57:55 +0300

    chore: update

--- projects/demo-cypress/cypress/tests/editor-api.cy.ts
+++ projects/demo-cypress/cypress/tests/editor-api.cy.ts
@@ -17,9 +17,7 @@ import {
 } from '../support/editor/helpers';
 import {
     HTML_EDITOR_EXAMPLE_BLOCKQUOTE,
-    HTML_EDITOR_EXAMPLE_DETAILS,
     HTML_EDITOR_EXAMPLE_HIGHLIGHT_BLOCK,
-    HTML_EDITOR_EXAMPLE_LEGACY_DETAILS,
     HTML_EDITOR_EXAMPLE_LONG_WORD_UL,
     HTML_EDITOR_EXAMPLE_NESTED_OL_UL,
     HTML_EDITOR_EXAMPLE_NESTED_UL,
@@ -81,27 +79,6 @@ describe(`Editor API`, () => {
                 .find(`tui-editor-socket.tui-example`)
                 .tuiWaitBeforeScreenshot()
                 .matchImageSnapshot(`2-1-dark-mode-output`);
-        });
-    });
-
-    describe(`details`, () => {
-        it(`prevents extra margin between top border of details and content`, () => {
-            tuiVisitEditorApiPage({content: HTML_EDITOR_EXAMPLE_DETAILS});
-
-            tuiGetDemoContent()
-                .tuiWaitBeforeScreenshot()
-                .matchImageSnapshot(`prevents-extra-margin-details`);
-        });
-
-        it(`legacy`, () => {
-            tuiVisitEditorApiPage({content: ``});
-
-            cy.get(`.t-table tr`)
-                .eq(2)
-                .findByAutomationId(`tui-primitive-textfield__native-input`)
-                .type(HTML_EDITOR_EXAMPLE_LEGACY_DETAILS, {force: true, delay: 0});
-
-            tuiGetDemoContent().tuiWaitBeforeScreenshot().matchImageSnapshot(`legacy`);
         });
     });
 
