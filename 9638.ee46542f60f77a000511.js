17a55273185389732e705f7aa98b38370af7729d
splincode <omaxphp@yandex.ru>
2024-03-12 11:30:20 +0000

    Update snapshots/demo/v1.x to output generated at 44ffd10

--- 9638.ee46542f60f77a000511.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunktui_editor=self.webpackChunktui_editor||[]).push([[9638],{89638:r=>{r.exports="```ts\nimport {platformBrowserDynamic} from '@angular/platform-browser-dynamic';\n\nimport {AppModule} from './app/app.module';\n\nplatformBrowserDynamic()\n  .bootstrapModule(AppModule)\n  .catch(err => console.error(err));\n```\n"}}]);
