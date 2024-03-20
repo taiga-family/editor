7aec33cb39dd2b8a4c8105d4dbeba107e9de678a
null <taiga-family-bot@yandex.ru>
2024-03-20 01:16:55 +0000

    chore(deps): update taiga-ui

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
+import {resolve} from 'node:path';
+
 import type {Config} from 'jest';
-import {resolve} from 'path';
 import {pathsToModuleNameMapper} from 'ts-jest';
 
 process.env.TZ = 'Europe/Moscow';
@@ -7,7 +8,7 @@ process.env.FORCE_COLOR = 'true';
 process.env.TS_JEST_DISABLE_VER_CHECKER = 'true';
 
 const {compilerOptions} = require(resolve(__dirname, 'tsconfig.json'));
-const maxParallel = require('os').cpus().length / 2;
+const maxParallel = require('node:os').cpus().length / 2;
 
 const config: Config = {
     rootDir: __dirname,
--- projects/demo/generate-routes.ts
+++ projects/demo/generate-routes.ts
@@ -1,5 +1,5 @@
-import {readFileSync, writeFileSync} from 'fs';
-import {join} from 'path';
+import {readFileSync, writeFileSync} from 'node:fs';
+import {join} from 'node:path';
 
 import {TuiDemoPath} from './src/app/constants';
 
