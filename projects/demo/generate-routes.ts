7e8bd83f5fa3dcddeb1fa7263c5902a664e86cad
Maksim Ivanov <omaxphp@yandex.ru>
2024-03-20 11:58:50 +0300

    feat: ssr (#883)

---
+++ projects/demo/generate-routes.ts
@@ -0,0 +1,49 @@
+import {readFileSync, writeFileSync} from 'fs';
+import {join} from 'path';
+
+import {TuiDemoPath} from './src/app/constants';
+
+const EXCEPTIONS: string[] = [TuiDemoPath.Stackblitz];
+
+/**
+ * This script is required for correct of `nx prerender demo` command.
+ * `@nguniversal/builders:prerender` (version 12) can't extract routes for lazy-loading modules.
+ * See: https://github.com/angular/universal/issues/1769
+ * ___
+ * TODO: after update to the newer version of Angular and `@nguniversal/builders`, check if this script is still required.
+ */
+(function main(): void {
+    const demoPathEnumContent = readFileSync(
+        join(
+            process.cwd(),
+            'projects',
+            'demo',
+            'src',
+            'app',
+            'constants',
+            'demo-path.ts',
+        ),
+        'utf-8',
+    );
+
+    const routes = ['/'];
+
+    const generated = (
+        demoPathEnumContent
+            .match(/['"`](.*)['"`]/g)
+            ?.map(route => route.replaceAll(/['"`]/g, '')) || []
+    )
+        .filter(route => !EXCEPTIONS.includes(route) && !route.includes('embed'))
+        .map(route => `/${route}`);
+
+    console.info('Generated routes:');
+
+    routes.push(...generated);
+
+    routes.forEach(route => console.info(`**** ${route}`));
+
+    writeFileSync(
+        join(process.cwd(), 'projects', 'demo', 'routes.txt'),
+        routes?.join('\n') || '',
+    );
+})();
