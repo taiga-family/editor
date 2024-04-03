a81f07945ac14cd18ce2d783e767a80c4b756862
splincode <omaxphp@yandex.ru>
2024-04-03 11:02:11 +0300

    fix: support preview full list of mention suggestion

--- projects/tui-editor/src/extensions/mention/index.ts
+++ projects/tui-editor/src/extensions/mention/index.ts
@@ -38,6 +38,10 @@ export const TuiMention = Node.create<TuiMentionOptions>({
                 parseHTML: element => element.innerText,
                 renderHTML: () => ({'data-type': this.name}),
             },
+            class: {
+                default: null,
+                keepOnSplit: true,
+            },
         };
     },
 
