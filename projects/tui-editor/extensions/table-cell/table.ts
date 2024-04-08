3f5864092cd4b1314949f3de1eefbce468bb336c
Maksim Ivanov <omaxphp@yandex.ru>
2024-04-08 13:39:52 +0300

    fix: incorrect column's width after cell merging (#962)

---
+++ projects/tui-editor/extensions/table-cell/table.ts
@@ -0,0 +1,14 @@
+import {mergeAttributes} from '@tiptap/core';
+import TableCell from '@tiptap/extension-table-cell';
+
+export const TuiTableCell = TableCell.extend({
+    renderHTML({HTMLAttributes}) {
+        const attrs = mergeAttributes(this.options.HTMLAttributes, HTMLAttributes);
+
+        if (attrs.colwidth) {
+            attrs.style = `width: ${attrs.colwidth}px`;
+        }
+
+        return [`td`, attrs, 0];
+    },
+});
