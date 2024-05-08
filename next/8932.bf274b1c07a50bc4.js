1b4c0114667b122e0d0f1e00191978249c8e806b
splincode <splincode@users.noreply.github.com>
2024-05-08 08:49:57 +0000

    Deploying to gh-pages from @ taiga-family/tui-editor@f3ab25e4e9349a9a86d765cb116e5985a6cc5778 🚀

--- next/8932.bf274b1c07a50bc4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[8932],{28932:o=>{o.exports='<tui-editor\n    class="editor"\n    [formControl]="control"\n    [tools]="builtInTools"\n/>\n\n<h4>HTML:</h4>\n<tui-editor-socket [content]="control.value || \'\'" />\n'}}]);
