f3ab25e4e9349a9a86d765cb116e5985a6cc5778
Maksim Ivanov <omaxphp@yandex.ru>
2024-05-08 11:46:59 +0300

    fix: class constructor NodeView cannot be invoked without 'new' (#1010)

--- projects/demo/src/app/pages/images/resizable/examples/1/index.ts
+++ projects/demo/src/app/pages/images/resizable/examples/1/index.ts
@@ -52,7 +52,7 @@ export default class ExampleComponent {
         this.base64Image$.pipe(takeUntilDestroyed()).subscribe(src => {
             this.control.patchValue(
                 `
-                    <img data-type="image-editor" src="${src}" width="300" alt="">
+                    <img src="${src}" width="300" alt="" />
                     <p>Try to drag right border of image!</p>
                     <p>To change min/max size of image use token <code>TUI_IMAGE_EDITOR_OPTIONS</code>.
                 `,
