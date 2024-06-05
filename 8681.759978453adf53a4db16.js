6ea60296e772bff3e84fd56eb3709c41ff33daf8
splincode <omaxphp@yandex.ru>
2024-06-05 12:32:36 +0000

    Update snapshots/demo/v1.x to output generated at 4d2a3b8

---
+++ 8681.759978453adf53a4db16.js
@@ -0,0 +1,1 @@
+(self.webpackChunktui_editor=self.webpackChunktui_editor||[]).push([[8681],{98681:o=>{o.exports="```typescript\nimport {defaultEditorExtensions, TUI_EDITOR_EXTENSIONS} from '@tbank/tui-editor';\n// ...\n\n@Component({\n  // ...\n})\nexport class AppComponent {\n  readonly control = new FormControl();\n\n  readonly tools: TuiEditorTool[] = defaultEditorTools;\n}\n```\n"}}]);
