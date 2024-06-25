8d40c0645dbae06a42e9875000ac91ac3490aa8c
splincode <splincode@users.noreply.github.com>
2024-06-25 18:01:49 +0000

    Deploying to gh-pages from @ taiga-family/editor@fbe2750ecfff1b1571c6165da5a342b3c9822ec1 🚀

--- v2/2954.e314bd7eb619624c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[2954],{42954:n=>{n.exports="[data-appearance='no-border'] {\n    &:after {\n        border: none;\n    }\n\n    .t-tools-wrapper {\n        flex-wrap: nowrap !important;\n        max-width: 25rem !important;\n    }\n}\n"}}]);
