db54e0ac0d4d257579e9c8e2fb90aebca0773005
splincode <omaxphp@yandex.ru>
2024-06-25 17:19:10 +0300

    chore(demo): update no appearance example

--- projects/demo/src/app/pages/appearance/examples/1/index.ts
+++ projects/demo/src/app/pages/appearance/examples/1/index.ts
@@ -1,18 +1,18 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {TuiActiveZone} from '@taiga-ui/cdk';
+import {TuiActiveZone, TuiAutoFocus} from '@taiga-ui/cdk';
 import {TuiEditor, tuiEditorOptionsProvider} from '@taiga-ui/editor';
 
 @Component({
     standalone: true,
-    imports: [TuiEditor, ReactiveFormsModule, TuiActiveZone],
+    imports: [TuiEditor, ReactiveFormsModule, TuiActiveZone, TuiAutoFocus],
     templateUrl: './index.html',
     styleUrls: ['./index.less'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [
         tuiEditorOptionsProvider({
-            appearence: 'no-border',
+            appearance: 'no-border',
         }),
     ],
 })
