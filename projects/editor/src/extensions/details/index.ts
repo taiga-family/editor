2896d9a623dc24af79b6adb1599073b5d7a63ba4
splincode <omaxphp@yandex.ru>
2024-07-17 13:22:52 +0300

    feat: support delete detail from contenteditable area

--- projects/editor/src/extensions/details/index.ts
+++ projects/editor/src/extensions/details/index.ts
@@ -64,24 +64,31 @@ export const TuiDetailsExtension = Node.create<TuiDetailsOptions>({
     },
 
     addNodeView() {
-        return ({node}): any => {
+        return ({node, editor}): any => {
             if (globalThis.document) {
                 const wrapper = document.createElement('div');
                 const details = document.createElement('details');
-                const button = document.createElement('button');
+                const collapseButton = document.createElement('button');
+                const deleteButton = document.createElement('button');
 
                 wrapper.className = 't-details-wrapper';
-                button.className = 't-details-arrow';
+                collapseButton.className = 't-details-arrow';
+                deleteButton.className = 't-details-delete';
 
                 details.open = node.attrs.opened;
 
-                button.addEventListener('click', () => {
+                collapseButton.addEventListener('click', () => {
                     details.open = !details.open;
                     (node.attrs as unknown as Record<string, unknown>).opened =
                         details.open;
                 });
 
-                wrapper.append(details, button);
+                deleteButton.addEventListener('click', () => {
+                    tuiDeleteNode(editor.state, editor.view.dispatch, this.name);
+                    editor.commands.focus('end');
+                });
+
+                wrapper.append(details, collapseButton, deleteButton);
 
                 return {
                     dom: wrapper,
@@ -104,8 +111,11 @@ export const TuiDetailsExtension = Node.create<TuiDetailsOptions>({
                 },
             removeDetails:
                 () =>
-                ({state, dispatch}) =>
-                    tuiDeleteNode(state, dispatch, this.name),
+                ({editor, state, dispatch}) => {
+                    tuiDeleteNode(state, dispatch, this.name);
+
+                    return editor.commands.focus('end');
+                },
         };
     },
 });
