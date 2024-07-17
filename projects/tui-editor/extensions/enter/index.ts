55c53da615f6e7181c62ee4fb36bd5a4ff39f9e5
splincode <omaxphp@yandex.ru>
2024-07-17 18:36:30 +0300

    feat: support break line inside details content

--- projects/tui-editor/extensions/enter/index.ts
+++ projects/tui-editor/extensions/enter/index.ts
@@ -5,10 +5,22 @@ export const TuiCustomEnter = Extension.create({
     addKeyboardShortcuts() {
         return {
             Enter: ({editor}) => {
-                (editor.commands as any)?.setHardBreak?.();
-                (editor.commands as any)?.setHardBreak?.();
+                if (editor.isActive(`summary`)) {
+                    editor.commands.selectNodeForward();
+                    editor?.commands.focus((editor?.state.selection.anchor ?? 0) + 1);
 
-                return true;
+                    if (globalThis.document) {
+                        editor.view
+                            .nodeDOM(editor.state.selection.anchor)
+                            ?.parentElement?.closest(`details`)
+                            ?.querySelector(`[data-type="details-content"]`)
+                            ?.prepend(document.createElement(`p`));
+                    }
+
+                    return false;
+                }
+
+                return this.editor.chain().createParagraphNear().run();
             },
         };
     },
