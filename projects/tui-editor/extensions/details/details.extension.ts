3a5c415f6264701b00997176ece3825c6c5573e1
splincode <omaxphp@yandex.ru>
2024-07-17 14:10:58 +0300

    feat: support delete detail from contenteditable area

--- projects/tui-editor/extensions/details/details.extension.ts
+++ projects/tui-editor/extensions/details/details.extension.ts
@@ -61,27 +61,37 @@ export const TuiDetails = Node.create<TuiDetailsOptions>({
     },
 
     addNodeView() {
-        return ({node}) => {
-            const wrapper = document.createElement(`div`);
-            const details = document.createElement(`details`);
-            const button = document.createElement(`button`);
+        return ({node, editor}): any => {
+            if (globalThis.document) {
+                const wrapper = document.createElement('div');
+                const details = document.createElement('details');
+                const collapseButton = document.createElement('button');
+                const deleteButton = document.createElement('button');
 
-            wrapper.className = `t-details-wrapper`;
-            button.className = `t-details-arrow`;
+                wrapper.className = 't-details-wrapper';
+                collapseButton.className = 't-details-arrow';
+                deleteButton.className = 't-details-delete';
 
-            details.open = node.attrs.opened;
+                details.open = node.attrs.opened;
 
-            button.addEventListener(`click`, () => {
-                details.open = !details.open;
-                (node.attrs as unknown as Record<string, unknown>).opened = details.open;
-            });
+                collapseButton.addEventListener('click', () => {
+                    details.open = !details.open;
+                    (node.attrs as unknown as Record<string, unknown>).opened =
+                        details.open;
+                });
 
-            wrapper.append(details, button);
+                deleteButton.addEventListener('click', () => {
+                    tuiDeleteNode(editor.state, editor.view.dispatch, this.name);
+                    editor.commands.focus('end');
+                });
 
-            return {
-                dom: wrapper,
-                contentDOM: details,
-            };
+                wrapper.append(details, collapseButton, deleteButton);
+
+                return {
+                    dom: wrapper,
+                    contentDOM: details,
+                };
+            }
         };
     },
 
@@ -98,8 +108,11 @@ export const TuiDetails = Node.create<TuiDetailsOptions>({
                 },
             removeDetails:
                 () =>
-                ({state, dispatch}) =>
-                    tuiDeleteNode(state, dispatch, this.name),
+                ({editor, state, dispatch}) => {
+                    tuiDeleteNode(state, dispatch, this.name);
+
+                    return editor.commands.focus('end');
+                },
         };
     },
 });
