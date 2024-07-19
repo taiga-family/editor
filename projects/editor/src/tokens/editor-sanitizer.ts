1a1ac831d10a304c5d82411cdb1a675a56ae360a
Maksim Ivanov <omaxphp@yandex.ru>
2024-07-19 11:43:23 +0300

    feat: support SSR (#1196)

--- projects/editor/src/tokens/editor-sanitizer.ts
+++ projects/editor/src/tokens/editor-sanitizer.ts
@@ -1,7 +1,6 @@
 import type {Sanitizer} from '@angular/core';
 import {tuiCreateTokenFromFactory} from '@taiga-ui/cdk';
-import {NgDompurifySanitizer} from '@taiga-ui/dompurify';
 
-export const TUI_EDITOR_SANITIZER = tuiCreateTokenFromFactory<Sanitizer>(
-    () => new NgDompurifySanitizer(),
+export const TUI_EDITOR_SANITIZER = tuiCreateTokenFromFactory<Sanitizer | null>(
+    () => null,
 );
