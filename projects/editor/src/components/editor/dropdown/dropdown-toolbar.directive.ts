73d218c01d9c2e4942d004440fdef1959f53cbf8
splincode <omaxphp@yandex.ru>
2024-07-22 11:24:50 +0300

    chore: update

--- projects/editor/src/components/editor/dropdown/dropdown-toolbar.directive.ts
+++ projects/editor/src/components/editor/dropdown/dropdown-toolbar.directive.ts
@@ -3,17 +3,12 @@ import type {OnDestroy} from '@angular/core';
 import {Directive, ElementRef, inject, Input, ViewContainerRef} from '@angular/core';
 import type {TuiBooleanHandler} from '@taiga-ui/cdk';
 import {
-    CHAR_NO_BREAK_SPACE,
-    CHAR_ZERO_WIDTH_SPACE,
     EMPTY_CLIENT_RECT,
     TUI_RANGE,
     TUI_TRUE_HANDLER,
-    tuiGetNativeFocused,
     tuiIsElement,
     tuiIsString,
-    tuiIsTextfield,
     tuiIsTextNode,
-    tuiPx,
 } from '@taiga-ui/cdk';
 import type {TuiRectAccessor} from '@taiga-ui/core';
 import {
@@ -21,7 +16,6 @@ import {
     tuiAsDriver,
     tuiAsRectAccessor,
     TuiDriver,
-    TuiDropdownDirective,
     tuiGetWordRange,
 } from '@taiga-ui/core';
 import {BehaviorSubject, combineLatest, distinctUntilChanged, map} from 'rxjs';
@@ -44,7 +38,6 @@ export class TuiEditorDropdownToolbar
     private readonly selection$ = inject(TUI_SELECTION_STREAM);
     private readonly el = inject(ElementRef<HTMLElement>);
     private readonly vcr = inject(ViewContainerRef);
-    private readonly dropdown = inject(TuiDropdownDirective);
 
     private readonly handler$ = new BehaviorSubject<TuiBooleanHandler<Range>>(
         TUI_TRUE_HANDLER,
@@ -70,7 +63,7 @@ export class TuiEditorDropdownToolbar
                     ? range
                     : this.range;
 
-            return (contained && handler(this.range)) || this.inDropdown(range);
+            return contained && handler(this.range);
         }),
     );
 
@@ -141,76 +134,11 @@ export class TuiEditorDropdownToolbar
     }
 
     private getRange(): Range {
-        const active = tuiGetNativeFocused(this.doc);
         const selection = this.doc.getSelection();
-        const range =
-            active && tuiIsTextfield(active) && this.el.nativeElement.contains(active)
-                ? this.veryVerySadInputFix(active)
-                : (selection?.rangeCount && selection.getRangeAt(0)) || this.range;
 
-        return range.cloneRange();
-    }
-
-    /**
-     * Check if Node is inside dropdown
-     */
-    private boxContains(node: Node): boolean {
-        return !!this.dropdown.dropdownBoxRef?.location.nativeElement.contains(node);
-    }
-
-    /**
-     * Check if given range is at least partially inside dropdown
-     */
-    private inDropdown(range: Range): boolean {
-        const {startContainer, endContainer} = range;
-        const {nativeElement} = this.el;
-        const inDropdown = this.boxContains(range.commonAncestorContainer);
-        const hostToDropdown =
-            this.boxContains(endContainer) && nativeElement.contains(startContainer);
-        const dropdownToHost =
-            this.boxContains(startContainer) && nativeElement.contains(endContainer);
-
-        return inDropdown || hostToDropdown || dropdownToHost;
-    }
-
-    private veryVerySadInputFix(element: HTMLInputElement | HTMLTextAreaElement): Range {
-        const {ghost = this.initGhost(element)} = this;
-        const {top, left, width, height} = element.getBoundingClientRect();
-        const {selectionStart, selectionEnd, value} = element;
-        const range = this.doc.createRange();
-        const hostRect = this.el.nativeElement.getBoundingClientRect();
-
-        ghost.style.top = tuiPx(top - hostRect.top);
-        ghost.style.left = tuiPx(left - hostRect.left);
-        ghost.style.width = tuiPx(width);
-        ghost.style.height = tuiPx(height);
-        ghost.textContent = CHAR_ZERO_WIDTH_SPACE + value + CHAR_NO_BREAK_SPACE;
-
-        range.setStart(ghost.firstChild as Node, selectionStart || 0);
-        range.setEnd(ghost.firstChild as Node, selectionEnd || 0);
-
-        return range;
-    }
-
-    /**
-     * Create an invisible DIV styled exactly like input/textarea element inside directive
-     */
-    private initGhost(element: HTMLInputElement | HTMLTextAreaElement): HTMLElement {
-        const ghost = this.doc.createElement('div');
-        const {font, letterSpacing, textTransform, padding} = getComputedStyle(element);
-
-        ghost.style.position = 'absolute';
-        ghost.style.pointerEvents = 'none';
-        ghost.style.opacity = '0';
-        ghost.style.whiteSpace = 'pre-wrap';
-        ghost.style.font = font;
-        ghost.style.letterSpacing = letterSpacing;
-        ghost.style.textTransform = textTransform;
-        ghost.style.padding = padding;
-
-        this.vcr.element.nativeElement.appendChild(ghost);
-        this.ghost = ghost;
-
-        return ghost;
+        return (
+            (selection?.rangeCount && selection.getRangeAt(0)) ||
+            this.range
+        )?.cloneRange();
     }
 }
