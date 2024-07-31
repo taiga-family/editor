bcb60c79cffd05274fdef7e5d6081d31206a59a1
splincode <splincode@users.noreply.github.com>
2024-07-31 15:59:18 +0000

    Deploying to gh-pages from @ taiga-family/editor@322f641f58cf875d81d2ee10137db7462c09676a 🚀

--- 8681.2b0ad036dfe9f67823cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunktui_editor=self.webpackChunktui_editor||[]).push([[8681],{98681:o=>{o.exports="```typescript\nimport {defaultEditorExtensions, TUI_EDITOR_EXTENSIONS} from '@tinkoff/tui-editor';\n// ...\n\n@Component({\n  // ...\n})\nexport class AppComponent {\n  readonly control = new FormControl();\n\n  readonly tools: TuiEditorTool[] = defaultEditorTools;\n}\n```\n"}}]);
---
+++ 8681.6da448b5fca9abc994e4.js
@@ -0,0 +1,1 @@
+(self.webpackChunktui_editor=self.webpackChunktui_editor||[]).push([[8681],{98681:o=>{o.exports="```typescript\nimport {defaultEditorTools} from '@tinkoff/tui-editor';\n// ...\n\n@Component({\n  // ...\n})\nexport class AppComponent {\n  readonly control = new FormControl();\n\n  readonly tools: TuiEditorTool[] = defaultEditorTools;\n}\n```\n"}}]);
