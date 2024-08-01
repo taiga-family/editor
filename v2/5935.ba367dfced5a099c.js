fe05c77b669811ca84b597fc3953af078f879fa5
splincode <splincode@users.noreply.github.com>
2024-08-01 15:39:35 +0000

    Deploying to gh-pages from @ taiga-family/editor@4d8d40ca714f22ea0ca3c95bb61e1c61972011bd 🚀

--- v2/5935.ba367dfced5a099c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[5935],{95935:e=>{e.exports=":host {\n    ::ng-deep .tui-editor-socket [data-type='group'] {\n        flex-direction: column;\n        padding: 0.5rem;\n        margin: 0.5rem 0;\n        border: 1px solid var(--tui-border-normal);\n    }\n}\n"}}]);
---
+++ v2/6205.430afb19a0d39b13.js
@@ -0,0 +1,1 @@
+(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[6205],{36205:e=>{e.exports=":host {\n    ::ng-deep .my-mention {\n        background: var(--tui-autofill);\n        border: 0.0625rem solid var(--tui-background-accent-opposite-hover);\n        border-radius: 0.4rem;\n        padding: 0.1rem 0.3rem;\n        cursor: default;\n    }\n}\n"}}]);
