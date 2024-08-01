58e2dceec10c3f7b22c6ed5f841729ab4fec2da6
splincode <omaxphp@yandex.ru>
2024-08-01 12:27:56 +0300

    feat: support align images in article

--- projects/tui-editor/extensions/image-editor/image-editor.module.ts
+++ projects/tui-editor/extensions/image-editor/image-editor.module.ts
@@ -1,37 +1,17 @@
 import {CommonModule} from '@angular/common';
-import {Directive, ElementRef, Inject, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {TuiActiveZoneModule} from '@taiga-ui/cdk';
 import {
-    tuiAsPositionAccessor,
     TuiButtonModule,
     TuiDataListModule,
     TuiDropdownModule,
     TuiHostedDropdownModule,
-    TuiPoint,
-    TuiPositionAccessor,
 } from '@taiga-ui/core';
 import {TuiDataListDropdownManagerModule} from '@taiga-ui/kit';
 import {TuiEditorResizableModule} from '@tinkoff/tui-editor/components/editor-resizable';
 
 import {TuiImageEditorComponent} from './image-editor.component';
-
-@Directive({
-    selector: `[topRight]`,
-    providers: [tuiAsPositionAccessor(TuiTopRightDirective)],
-})
-export class TuiTopRightDirective extends TuiPositionAccessor {
-    readonly type = `dropdown`;
-
-    constructor(@Inject(ElementRef) private readonly el: ElementRef<HTMLElement>) {
-        super();
-    }
-
-    getPosition({width, height}: ClientRect): TuiPoint {
-        const {right, top} = this.el.nativeElement.getBoundingClientRect();
-
-        return [top + height + 5, right - width / 2];
-    }
-}
+import {TuiImageOptionsPositionDirective} from './image-options-position.directive';
 
 @NgModule({
     imports: [
@@ -44,7 +24,7 @@ export class TuiTopRightDirective extends TuiPositionAccessor {
         TuiDropdownModule,
         TuiDataListModule,
     ],
-    declarations: [TuiImageEditorComponent, TuiTopRightDirective],
+    declarations: [TuiImageEditorComponent, TuiImageOptionsPositionDirective],
     exports: [TuiImageEditorComponent],
 })
 export class TuiImageEditorModule {}
---
+++ projects/tui-editor/extensions/image-editor/image-options-position.directive.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef, Inject} from '@angular/core';
+import {tuiAsPositionAccessor, TuiPoint, TuiPositionAccessor} from '@taiga-ui/core';
+
+@Directive({
+    selector: `[tuiImageOptionsPosition]`,
+    providers: [tuiAsPositionAccessor(TuiImageOptionsPositionDirective)],
+})
+export class TuiImageOptionsPositionDirective extends TuiPositionAccessor {
+    readonly type = `dropdown`;
+
+    constructor(@Inject(ElementRef) private readonly el: ElementRef<HTMLElement>) {
+        super();
+    }
+
+    getPosition({width, height}: ClientRect): TuiPoint {
+        const {right, top} = this.el.nativeElement.getBoundingClientRect();
+
+        return [top + height + 5, right - width / 2];
+    }
+}
