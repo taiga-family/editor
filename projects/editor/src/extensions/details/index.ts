0f29fe5102674f25ccd5f2e6f7e62c245df50a2c
splincode <omaxphp@yandex.ru>
2024-08-06 22:05:52 +0300

    chore: update

--- projects/editor/src/extensions/details/index.ts
+++ projects/editor/src/extensions/details/index.ts
@@ -95,6 +95,8 @@ export const TuiDetailsExtension = Node.create<TuiDetailsOptions>({
                     contentDOM: details,
                 };
             }
+
+            return null;
         };
     },
 
