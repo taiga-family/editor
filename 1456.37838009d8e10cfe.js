30b79a7c65050efa75eccda1a2d90c5c077d2f42
splincode <omaxphp@yandex.ru>
2024-09-05 14:40:54 +0000

    Deploying to gh-pages from @ taiga-family/editor@74e1b05647dc2eed8154e7f52b63ea1dfec91200 🚀

--- 1456.37838009d8e10cfe.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[1456],{1456:e=>{e.exports="tui-editor.no-border {\n    border: none;\n\n    [data-appearance='no-border'] {\n        outline: none;\n    }\n}\n"}}]);
--- 5212.47d56089f3ce717e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkeditor_demo=self.webpackChunkeditor_demo||[]).push([[5212],{5212:o=>{o.exports='<tui-editor\n    tuiAutoFocus\n    class="no-border"\n    [formControl]="control"\n    [tools]="[]"\n/>\n'}}]);
