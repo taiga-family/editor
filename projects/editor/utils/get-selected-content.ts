00ce1256e882e483cfc56e0c775ebdb3a0e0053e
splincode <omaxphp@yandex.ru>
2025-03-14 17:14:33 +0300

    fix: cannot read properties of undefined `reading nodeSize`

--- projects/editor/utils/get-selected-content.ts
+++ projects/editor/utils/get-selected-content.ts
@@ -3,7 +3,7 @@ import type {EditorState} from '@tiptap/pm/state';
 
 export function tuiGetSelectedContent(state: EditorState, current?: string): string {
     const currentNodeContent = current ?? state.selection.$head.parent.textContent;
-    const selected = state.doc.cut(state.selection.from, state.selection.to);
+    const selected = state.tr.doc.cut(state.tr.selection.from, state.tr.selection.to);
 
     return selected.content.size
         ? getHTMLFromFragment(selected.content, state.schema)
