d1f55bff2fe54af7b621c1e841f97887359bc899
splincode <omaxphp@yandex.ru>
2025-06-26 17:15:54 +0300

    fix: support align toolbar by bottom

---
+++ projects/demo-playwright/tests/demo.spec.ts
@@ -0,0 +1,46 @@
+import {expect, test} from '@playwright/test';
+
+import {TuiDemoPath} from '../../demo/src/app/shared/routes';
+import {tuiGoto} from '../utils';
+
+test.describe('Demo', () => {
+    const IGNORE_FLAKY = new Set<string>([
+        TuiDemoPath.EmbedIframe,
+        TuiDemoPath.EmbedPDF,
+        TuiDemoPath.EmbedYoutube,
+        TuiDemoPath.StarterKit,
+    ]);
+
+    Object.entries(TuiDemoPath)
+        .filter(([, path]) => !IGNORE_FLAKY.has(path))
+        .forEach(([name, path]) => {
+            test(name, async ({page}) => {
+                await tuiGoto(page, path);
+
+                await expect(async () => {
+                    const examples = await page.locator('tui-doc-example').all();
+
+                    expect(examples.length).toBeGreaterThan(0);
+
+                    for (const example of examples) {
+                        await expect(
+                            example.getByTestId('tui-doc-example'),
+                        ).toBeAttached();
+                    }
+                }).toPass();
+
+                const examples = await page.getByTestId('tui-doc-example').all();
+
+                for (const [i, example] of examples.entries()) {
+                    await example.scrollIntoViewIfNeeded();
+
+                    await expect
+                        .soft(example)
+                        .toHaveScreenshot(
+                            [path.replace('/', '').replaceAll('/', '-'), `${i + 1}.png`],
+                            {timeout: 5 * 60 * 1000 * 2},
+                        );
+                }
+            });
+        });
+});
