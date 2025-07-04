309feafe1b8f18b484c4eb2bbfa9222ab8da3aa5
splincode <omaxphp@yandex.ru>
2025-07-04 12:58:33 +0300

    chore(demo): add details example

---
+++ projects/demo/src/app/pages/details/examples/1/index.ts
@@ -0,0 +1,55 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {
+    TUI_EDITOR_EXTENSIONS,
+    TuiEditor,
+    TuiEditorSocket,
+    TuiEditorTool,
+} from '@taiga-ui/editor';
+
+@Component({
+    standalone: true,
+    imports: [ReactiveFormsModule, TuiEditor, TuiEditorSocket],
+    templateUrl: './index.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        {
+            provide: TUI_EDITOR_EXTENSIONS,
+            useValue: [
+                import('@taiga-ui/editor').then(({TuiStarterKit}) => TuiStarterKit),
+                import('@tiptap/extension-text-style').then(({TextStyle}) => TextStyle),
+                import('@taiga-ui/editor').then(
+                    ({TuiDetailsExtension}) => TuiDetailsExtension,
+                ),
+                import('@taiga-ui/editor').then(({TuiSummary}) => TuiSummary),
+                import('@taiga-ui/editor').then(
+                    ({TuiDetailsContent}) => TuiDetailsContent,
+                ),
+            ],
+        },
+    ],
+})
+export default class Example {
+    protected builtInTools = [
+        TuiEditorTool.Undo,
+        TuiEditorTool.Size,
+        TuiEditorTool.Bold,
+        TuiEditorTool.Italic,
+        TuiEditorTool.Underline,
+        TuiEditorTool.Details,
+    ];
+
+    protected control = new FormControl(`
+        <details open>
+            <summary><p>Title 1</p></summary>
+            <p>Content 1</p>
+        </details>
+
+        <p>Hello world</p>
+
+        <details>
+            <summary><p>Title 2</p></summary>
+            <p>Content 2</p>
+        </details>
+    `);
+}
---
+++ projects/demo/src/app/pages/details/index.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiAddonDoc} from '@taiga-ui/addon-doc';
+
+@Component({
+    standalone: true,
+    imports: [TuiAddonDoc],
+    templateUrl: './index.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class Example {
+    protected readonly component1 = import('./examples/1');
+    protected readonly example1 = {
+        TypeScript: import('./examples/1/index.ts?raw'),
+        HTML: import('./examples/1/index.html?raw'),
+    };
+}
