fb34eb955a6dcf3e793b04f70fbb0dad1af1b930
splincode <omaxphp@yandex.ru>
2026-05-07 14:44:30 +0000

    Update snapshots/demo/v4.x to output generated at d54d090

---
+++ chunk-TOFTY7LT.js
@@ -0,0 +1,73 @@
+import {
+  TuiAddonDoc,
+  TuiDocExample,
+  TuiDocPage
+} from "./chunk-5OYCAE6U.js";
+import "./chunk-N2I4NDEC.js";
+import "./chunk-43T2765T.js";
+import "./chunk-RGU5X6JK.js";
+import "./chunk-6VH6DR7W.js";
+import "./chunk-GLG7L24I.js";
+import "./chunk-TO6IF4KK.js";
+import "./chunk-2LK3XWNF.js";
+import "./chunk-UK3D2IJN.js";
+import "./chunk-CZCJZTAA.js";
+import "./chunk-LTRKP6XS.js";
+import "./chunk-UO32WQJY.js";
+import "./chunk-XQBFNBOU.js";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  setClassMetadata,
+  ɵsetClassDebugInfo,
+  ɵɵadvance,
+  ɵɵdefineComponent,
+  ɵɵelement,
+  ɵɵelementEnd,
+  ɵɵelementStart,
+  ɵɵproperty
+} from "./chunk-QZKNXBAU.js";
+import "./chunk-4MWRP73S.js";
+
+// projects/demo/src/app/pages/embed/youtube/index.ts
+var _Example = class _Example {
+  constructor() {
+    this.component1 = import("./chunk-X6QO2SMN.js");
+    this.example1 = {
+      HTML: import("./chunk-O3FA4TWN.js"),
+      TypeScript: import("./chunk-FTNAIUTC.js"),
+      LESS: import("./chunk-NC7ME2FV.js"),
+      "youtube-tool/youtube-tool.component.ts": import("./chunk-HKLHBWO2.js"),
+      "youtube-tool/youtube-tool.template.html": import("./chunk-WMCIMKOL.js"),
+      "youtube-tool/youtube-tool.styles.less": import("./chunk-PL2X3DEW.js")
+    };
+  }
+};
+_Example.\u0275fac = function Example_Factory(__ngFactoryType__) {
+  return new (__ngFactoryType__ || _Example)();
+};
+_Example.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _Example, selectors: [["ng-component"]], decls: 2, vars: 2, consts: [["heading", "Youtube extension", 3, "component", "content"]], template: function Example_Template(rf, ctx) {
+  if (rf & 1) {
+    \u0275\u0275elementStart(0, "tui-doc-page");
+    \u0275\u0275element(1, "tui-doc-example", 0);
+    \u0275\u0275elementEnd();
+  }
+  if (rf & 2) {
+    \u0275\u0275advance();
+    \u0275\u0275property("component", ctx.component1)("content", ctx.example1);
+  }
+}, dependencies: [TuiDocExample, TuiDocPage], encapsulation: 2, changeDetection: 0 });
+var Example = _Example;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(Example, [{
+    type: Component,
+    args: [{ imports: [TuiAddonDoc], changeDetection: ChangeDetectionStrategy.OnPush, template: '<tui-doc-page>\n    <tui-doc-example\n        heading="Youtube extension"\n        [component]="component1"\n        [content]="example1"\n    />\n</tui-doc-page>\n' }]
+  }], null, null);
+})();
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && \u0275setClassDebugInfo(Example, { className: "Example", filePath: "projects/demo/src/app/pages/embed/youtube/index.ts", lineNumber: 9 });
+})();
+export {
+  Example as default
+};
+//# sourceMappingURL=chunk-TOFTY7LT.js.map
---
+++ chunk-WYZOGDF5.js
@@ -0,0 +1,117 @@
+import {
+  TuiEditor,
+  provideTuiEditor
+} from "./chunk-LDD6Y27D.js";
+import "./chunk-WCJAXZXO.js";
+import "./chunk-CD3ZDPWT.js";
+import "./chunk-J5UAEM45.js";
+import "./chunk-ZB2SK7NT.js";
+import "./chunk-KFJ6GJTL.js";
+import "./chunk-PJJ4EG5U.js";
+import "./chunk-M6TSJ4NT.js";
+import "./chunk-LR6MFXG2.js";
+import "./chunk-N3XZP6Z7.js";
+import "./chunk-6JDH2PV3.js";
+import "./chunk-X7NG6AFM.js";
+import "./chunk-7QK2CPSE.js";
+import "./chunk-WBHAP4JC.js";
+import "./chunk-X3PSOWX2.js";
+import "./chunk-E3TKWIMT.js";
+import "./chunk-HSMAYE7Z.js";
+import "./chunk-ARYGUNWN.js";
+import "./chunk-3L6ILX4W.js";
+import "./chunk-K3KVBV5Y.js";
+import "./chunk-BAXR2EDS.js";
+import "./chunk-3MA5NXQZ.js";
+import "./chunk-IG5NUA4T.js";
+import "./chunk-LPOWL4X5.js";
+import "./chunk-RURN6JQY.js";
+import "./chunk-QOV567V2.js";
+import "./chunk-SB5PRQWC.js";
+import "./chunk-G3QJNC4B.js";
+import "./chunk-7CX5NSUY.js";
+import "./chunk-RS2S3DLU.js";
+import "./chunk-ZL2NLOVI.js";
+import "./chunk-WQSN4Q62.js";
+import "./chunk-F47ZTIMH.js";
+import "./chunk-H6B6ZN6C.js";
+import "./chunk-VCEUECWR.js";
+import "./chunk-EHYODUHD.js";
+import "./chunk-TO6IF4KK.js";
+import "./chunk-2LK3XWNF.js";
+import "./chunk-UK3D2IJN.js";
+import "./chunk-LTRKP6XS.js";
+import "./chunk-AHSN3FX6.js";
+import "./chunk-OR7YRS4P.js";
+import "./chunk-2X7NNA2Q.js";
+import {
+  TuiEditorTool
+} from "./chunk-WUF6K6RJ.js";
+import "./chunk-WLOLI4OQ.js";
+import "./chunk-UO32WQJY.js";
+import {
+  FormControl,
+  FormControlDirective,
+  NgControlStatus,
+  ReactiveFormsModule
+} from "./chunk-XQBFNBOU.js";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  setClassMetadata,
+  ɵsetClassDebugInfo,
+  ɵɵProvidersFeature,
+  ɵɵdefineComponent,
+  ɵɵelement,
+  ɵɵproperty
+} from "./chunk-QZKNXBAU.js";
+import "./chunk-TPMK677Z.js";
+import "./chunk-Q6WMBPZ3.js";
+import "./chunk-IN2LCJCD.js";
+import "./chunk-U65UEYRO.js";
+import "./chunk-4MWRP73S.js";
+
+// projects/demo/src/app/pages/font/examples/4/index.ts
+var _Example = class _Example {
+  constructor() {
+    this.builtInTools = [TuiEditorTool.Undo];
+    this.control = new FormControl("<p>Hello</p>");
+  }
+};
+_Example.\u0275fac = function Example_Factory(__ngFactoryType__) {
+  return new (__ngFactoryType__ || _Example)();
+};
+_Example.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _Example, selectors: [["ng-component"]], features: [\u0275\u0275ProvidersFeature([
+  provideTuiEditor({
+    bold: false,
+    italic: false,
+    strike: false
+  })
+])], decls: 1, vars: 2, consts: [[3, "formControl", "tools"]], template: function Example_Template(rf, ctx) {
+  if (rf & 1) {
+    \u0275\u0275element(0, "tui-editor", 0);
+  }
+  if (rf & 2) {
+    \u0275\u0275property("formControl", ctx.control)("tools", ctx.builtInTools);
+  }
+}, dependencies: [ReactiveFormsModule, NgControlStatus, FormControlDirective, TuiEditor], encapsulation: 2, changeDetection: 0 });
+var Example = _Example;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(Example, [{
+    type: Component,
+    args: [{ imports: [ReactiveFormsModule, TuiEditor], changeDetection: ChangeDetectionStrategy.OnPush, providers: [
+      provideTuiEditor({
+        bold: false,
+        italic: false,
+        strike: false
+      })
+    ], template: '<tui-editor\n    [formControl]="control"\n    [tools]="builtInTools"\n/>\n' }]
+  }], null, null);
+})();
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && \u0275setClassDebugInfo(Example, { className: "Example", filePath: "projects/demo/src/app/pages/font/examples/4/index.ts", lineNumber: 17 });
+})();
+export {
+  Example as default
+};
+//# sourceMappingURL=chunk-WYZOGDF5.js.map
