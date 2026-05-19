920a53a5caaec5fa5958b3d46df81fcb8cc17a9c
splincode <omaxphp@yandex.ru>
2026-05-19 04:29:11 +0000

    Update snapshots/demo/v5.x to output generated at 6257bae

---
+++ chunk-OQ3OE5CA.js
@@ -0,0 +1,14 @@
+import {
+  TUI_EDITOR_GROUP_DEFAULT_OPTIONS,
+  tuiCreateGroupExtension
+} from "./chunk-RPYTK5KS.js";
+import "./chunk-CVRSSI2P.js";
+import "./chunk-6HH4HFWJ.js";
+import "./chunk-NS3244ZP.js";
+import "./chunk-77B4UB4S.js";
+import "./chunk-KWSTWQNB.js";
+export {
+  TUI_EDITOR_GROUP_DEFAULT_OPTIONS,
+  tuiCreateGroupExtension
+};
+//# sourceMappingURL=chunk-OQ3OE5CA.js.map
