b34cc1ce290679d5b5dd7ff0b7302b83c94dc963
splincode <omaxphp@yandex.ru>
2026-06-07 04:17:29 +0000

    Update snapshots/demo/next/main to output generated at 74f3460

---
+++ chunk-FBSWDQFU.js
@@ -0,0 +1,89 @@
+import {
+  TuiAddonDoc,
+  TuiDocExample,
+  TuiDocPage
+} from "./chunk-SJ3SG3PG.js";
+import "./chunk-RFPAUGZU.js";
+import "./chunk-EX75VRI7.js";
+import "./chunk-74E46TAL.js";
+import "./chunk-QBSXUJOT.js";
+import "./chunk-FPB4G23O.js";
+import "./chunk-CDSTSEQI.js";
+import {
+  TuiLink
+} from "./chunk-462RAKEG.js";
+import "./chunk-LPBSAZLJ.js";
+import "./chunk-HHFYZN44.js";
+import "./chunk-DMAZJOQQ.js";
+import "./chunk-EMEGSUC4.js";
+import "./chunk-5BSJ6YPL.js";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  setClassMetadata,
+  ɵsetClassDebugInfo,
+  ɵɵadvance,
+  ɵɵdefineComponent,
+  ɵɵelementEnd,
+  ɵɵelementStart,
+  ɵɵproperty,
+  ɵɵreference,
+  ɵɵtemplate,
+  ɵɵtemplateRefExtractor,
+  ɵɵtext
+} from "./chunk-KFKFA6WT.js";
+import "./chunk-KWSTWQNB.js";
+
+// projects/demo/src/app/pages/table-of-contents/index.ts
+function Example_ng_template_2_Template(rf, ctx) {
+  if (rf & 1) {
+    \u0275\u0275elementStart(0, "p");
+    \u0275\u0275text(1, " The ");
+    \u0275\u0275elementStart(2, "a", 2);
+    \u0275\u0275text(3, " TableOfContents ");
+    \u0275\u0275elementEnd();
+    \u0275\u0275text(4, " extension lets you get a list of anchors from your document and passes on important information about each anchor (for example the depth, the content and a unique ID for each heading but also the active state and scroll states for each anchor). This can be used to render the table of content on your own. ");
+    \u0275\u0275elementEnd();
+  }
+}
+var _Example = class _Example {
+  constructor() {
+    this.component1 = import("./chunk-2XWIYLEM.js");
+    this.example1 = {
+      TypeScript: import("./chunk-OQKOBRES.js"),
+      LESS: import("./chunk-LCDCY44O.js"),
+      "./my-toc.html": import("./chunk-2C5RD2J2.js"),
+      "./my-toc.ts": import("./chunk-HS4JLUZS.js"),
+      "./my-toc.less": import("./chunk-ENY26VVQ.js")
+    };
+  }
+};
+_Example.\u0275fac = function Example_Factory(__ngFactoryType__) {
+  return new (__ngFactoryType__ || _Example)();
+};
+_Example.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _Example, selectors: [["ng-component"]], decls: 4, vars: 3, consts: [["description", ""], ["heading", "Table of contents", 3, "component", "content", "description"], ["href", "https://tiptap.dev/docs/editor/extensions/functionality/table-of-contents", "target", "_blank", "tuiLink", ""]], template: function Example_Template(rf, ctx) {
+  if (rf & 1) {
+    \u0275\u0275elementStart(0, "tui-doc-page")(1, "tui-doc-example", 1);
+    \u0275\u0275template(2, Example_ng_template_2_Template, 5, 0, "ng-template", null, 0, \u0275\u0275templateRefExtractor);
+    \u0275\u0275elementEnd()();
+  }
+  if (rf & 2) {
+    const description_r1 = \u0275\u0275reference(3);
+    \u0275\u0275advance();
+    \u0275\u0275property("component", ctx.component1)("content", ctx.example1)("description", description_r1);
+  }
+}, dependencies: [TuiDocExample, TuiDocPage, TuiLink], encapsulation: 2, changeDetection: 0 });
+var Example = _Example;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(Example, [{
+    type: Component,
+    args: [{ imports: [TuiAddonDoc, TuiLink], changeDetection: ChangeDetectionStrategy.OnPush, template: '<tui-doc-page>\n    <tui-doc-example\n        heading="Table of contents"\n        [component]="component1"\n        [content]="example1"\n        [description]="description"\n    >\n        <ng-template #description>\n            <p>\n                The\n                <a\n                    href="https://tiptap.dev/docs/editor/extensions/functionality/table-of-contents"\n                    target="_blank"\n                    tuiLink\n                >\n                    TableOfContents\n                </a>\n                extension lets you get a list of anchors from your document and passes on important information about\n                each anchor (for example the depth, the content and a unique ID for each heading but also the active\n                state and scroll states for each anchor). This can be used to render the table of content on your own.\n            </p>\n        </ng-template>\n    </tui-doc-example>\n</tui-doc-page>\n' }]
+  }], null, null);
+})();
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && \u0275setClassDebugInfo(Example, { className: "Example", filePath: "projects/demo/src/app/pages/table-of-contents/index.ts", lineNumber: 10 });
+})();
+export {
+  Example as default
+};
+//# sourceMappingURL=chunk-FBSWDQFU.js.map
---
+++ chunk-PS4MTOYC.js
@@ -0,0 +1,151 @@
+import {
+  UploadService,
+  fileLoader
+} from "./chunk-J5COV7YV.js";
+import {
+  TuiEditor,
+  TuiToolbar,
+  provideTuiEditor
+} from "./chunk-ZDERITJE.js";
+import "./chunk-4UYSEWYM.js";
+import "./chunk-ON6CMRJE.js";
+import "./chunk-CGQ7SF5X.js";
+import "./chunk-K4VHZDJ3.js";
+import "./chunk-BMQKW5MW.js";
+import "./chunk-AJXXSTGP.js";
+import "./chunk-PZFOFBEJ.js";
+import "./chunk-AVXWN6PC.js";
+import "./chunk-DFL5NIEH.js";
+import "./chunk-BMTZ6XJZ.js";
+import "./chunk-LEV3RHMX.js";
+import "./chunk-4P4YSWKO.js";
+import "./chunk-ZRZNAMTC.js";
+import "./chunk-ZPZIXJAI.js";
+import "./chunk-2RUHTAHV.js";
+import "./chunk-3N57KBZI.js";
+import {
+  TuiLoader
+} from "./chunk-CDSTSEQI.js";
+import "./chunk-462RAKEG.js";
+import "./chunk-LPBSAZLJ.js";
+import "./chunk-DMAZJOQQ.js";
+import "./chunk-QVEY2JIK.js";
+import "./chunk-KMEEBF2K.js";
+import "./chunk-MAMGYOWB.js";
+import {
+  TUI_ATTACH_FILES_LOADER,
+  TuiEditorTool
+} from "./chunk-XDT5UXUV.js";
+import "./chunk-HNVQLPMS.js";
+import "./chunk-EMEGSUC4.js";
+import {
+  FormControl,
+  FormControlDirective,
+  NgControlStatus,
+  ReactiveFormsModule
+} from "./chunk-5BSJ6YPL.js";
+import {
+  AsyncPipe,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  setClassMetadata,
+  ɵsetClassDebugInfo,
+  ɵɵProvidersFeature,
+  ɵɵadvance,
+  ɵɵdefineComponent,
+  ɵɵelementEnd,
+  ɵɵelementStart,
+  ɵɵgetCurrentView,
+  ɵɵlistener,
+  ɵɵpipe,
+  ɵɵpipeBind1,
+  ɵɵproperty,
+  ɵɵpureFunction0,
+  ɵɵreference,
+  ɵɵresetView,
+  ɵɵrestoreView
+} from "./chunk-KFKFA6WT.js";
+import "./chunk-OT66PQDS.js";
+import "./chunk-6E3PG6FD.js";
+import "./chunk-GAZHX25U.js";
+import "./chunk-QAKA6OWS.js";
+import "./chunk-KWSTWQNB.js";
+
+// projects/demo/src/app/pages/upload-files/examples/2/index.ts
+var _c0 = () => [];
+var _Example = class _Example {
+  constructor() {
+    this.editorRef = null;
+    this.uploadService = inject(UploadService);
+    this.builtInTools = [
+      TuiEditorTool.Undo,
+      TuiEditorTool.Link,
+      TuiEditorTool.Attach
+    ];
+    this.control = new FormControl("");
+  }
+  attach(files) {
+    files.forEach((file) => this.editorRef?.setFileLink(file));
+  }
+};
+_Example.\u0275fac = function Example_Factory(__ngFactoryType__) {
+  return new (__ngFactoryType__ || _Example)();
+};
+_Example.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _Example, selectors: [["ng-component"]], features: [\u0275\u0275ProvidersFeature([
+  provideTuiEditor({ link: { protocols: ["blob"] } }),
+  UploadService,
+  {
+    provide: TUI_ATTACH_FILES_LOADER,
+    deps: [UploadService],
+    useFactory: fileLoader
+  }
+])], decls: 5, vars: 8, consts: [["editorComponent", ""], [1, "toolbar", 3, "fileAttached", "editor", "tools"], [3, "loading"], [1, "editor", 3, "focusIn", "formControl", "tools"]], template: function Example_Template(rf, ctx) {
+  if (rf & 1) {
+    const _r1 = \u0275\u0275getCurrentView();
+    \u0275\u0275elementStart(0, "tui-toolbar", 1);
+    \u0275\u0275listener("fileAttached", function Example_Template_tui_toolbar_fileAttached_0_listener($event) {
+      \u0275\u0275restoreView(_r1);
+      return \u0275\u0275resetView(ctx.attach($event));
+    });
+    \u0275\u0275elementEnd();
+    \u0275\u0275elementStart(1, "tui-loader", 2);
+    \u0275\u0275pipe(2, "async");
+    \u0275\u0275elementStart(3, "tui-editor", 3, 0);
+    \u0275\u0275listener("focusIn", function Example_Template_tui_editor_focusIn_3_listener() {
+      \u0275\u0275restoreView(_r1);
+      const editorComponent_r2 = \u0275\u0275reference(4);
+      return \u0275\u0275resetView(ctx.editorRef = editorComponent_r2.editor);
+    });
+    \u0275\u0275elementEnd()();
+  }
+  if (rf & 2) {
+    \u0275\u0275property("editor", ctx.editorRef)("tools", ctx.builtInTools);
+    \u0275\u0275advance();
+    \u0275\u0275property("loading", !!\u0275\u0275pipeBind1(2, 5, ctx.uploadService.loading$));
+    \u0275\u0275advance(2);
+    \u0275\u0275property("formControl", ctx.control)("tools", \u0275\u0275pureFunction0(7, _c0));
+  }
+}, dependencies: [AsyncPipe, ReactiveFormsModule, NgControlStatus, FormControlDirective, TuiEditor, TuiLoader, TuiToolbar], styles: ["\n\n.toolbar[_ngcontent-%COMP%] {\n  margin-block-end: 0.5rem;\n}\n.editor[_ngcontent-%COMP%]     .tui-editor-socket .ProseMirror {\n  min-block-size: 10rem;\n}\n/*# sourceMappingURL=index.css.map */"], changeDetection: 0 });
+var Example = _Example;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(Example, [{
+    type: Component,
+    args: [{ imports: [AsyncPipe, ReactiveFormsModule, TuiEditor, TuiLoader, TuiToolbar], changeDetection: ChangeDetectionStrategy.OnPush, providers: [
+      provideTuiEditor({ link: { protocols: ["blob"] } }),
+      UploadService,
+      {
+        provide: TUI_ATTACH_FILES_LOADER,
+        deps: [UploadService],
+        useFactory: fileLoader
+      }
+    ], template: '<tui-toolbar\n    class="toolbar"\n    [editor]="editorRef"\n    [tools]="builtInTools"\n    (fileAttached)="attach($event)"\n/>\n\n<tui-loader [loading]="!!(uploadService.loading$ | async)">\n    <tui-editor\n        #editorComponent\n        class="editor"\n        [formControl]="control"\n        [tools]="[]"\n        (focusIn)="editorRef = editorComponent.editor"\n    />\n</tui-loader>\n', styles: ["/* projects/demo/src/app/pages/upload-files/examples/2/index.less */\n.toolbar {\n  margin-block-end: 0.5rem;\n}\n.editor ::ng-deep .tui-editor-socket .ProseMirror {\n  min-block-size: 10rem;\n}\n/*# sourceMappingURL=index.css.map */\n"] }]
+  }], null, null);
+})();
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && \u0275setClassDebugInfo(Example, { className: "Example", filePath: "projects/demo/src/app/pages/upload-files/examples/2/index.ts", lineNumber: 33 });
+})();
+export {
+  Example as default
+};
+//# sourceMappingURL=chunk-PS4MTOYC.js.map
