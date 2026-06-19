65d9698f9adf9dea273fee6e3d6691687c4fa366
splincode <omaxphp@yandex.ru>
2026-06-19 10:05:44 +0000

    Deploying to gh-pages from @ taiga-family/editor@ce89e6526441dda62e65abc4a26b7d762318e73d 🚀

---
+++ chunk-GCICTXHS.js
@@ -0,0 +1,86 @@
+import{Da as N,ia as M,n as F}from"./chunk-B5HNVDLR.js";import"./chunk-SBOE65Y5.js";import"./chunk-SNUH4AOV.js";import"./chunk-M4MKLJ5X.js";import"./chunk-FELRUIV7.js";import"./chunk-LKCF5GKV.js";import"./chunk-XGTPHHTH.js";import"./chunk-EWYWD752.js";import"./chunk-SMK37PTV.js";import"./chunk-I4VRDTI4.js";import"./chunk-NZSR3CAL.js";import"./chunk-YYHRYFGD.js";import"./chunk-KBDJOIOA.js";import"./chunk-7MSEWO54.js";import"./chunk-QFXPZ7U5.js";import"./chunk-CHAM34MP.js";import"./chunk-5BF7MNKE.js";import{a as _,b as A,c as D}from"./chunk-S5CRWCU5.js";import"./chunk-XHW4HFK6.js";import{d as S}from"./chunk-FQMJRYNZ.js";import{ea as g}from"./chunk-3TKVAUT4.js";import"./chunk-KGUQFVBY.js";import"./chunk-TIWPNJCX.js";import"./chunk-QZ7HZJYC.js";import"./chunk-37EH4AC6.js";import"./chunk-BERO5W6C.js";import{c}from"./chunk-QRYHZ6ZG.js";import"./chunk-HAHOGPVR.js";import"./chunk-X52YA4KS.js";import{D as I,r as E,u as T,z as C}from"./chunk-77SMMHKG.js";import{$b as o,Bc as h,Eb as b,Kb as s,Rb as i,_b as a,ac as m,bc as u,cc as f,hc as v,pb as t,qa as x,rc as r,sc as y}from"./chunk-SWLXYIRQ.js";import"./chunk-BFKBIB36.js";import"./chunk-LPOGFM6N.js";import"./chunk-DUYY2UWQ.js";import"./chunk-RHWM3AIW.js";import"./chunk-DAQOROHW.js";function L(p,d){if(p&1&&(u(0),m(1,"tui-editor-socket",4),f()),p&2){let e=v();t(),i("content",e.control.value)}}function P(p,d){if(p&1&&(u(0),a(1,"span",5),r(2),o(),f()),p&2){let e=v();t(2),y(e.control.value)}}var n=class n{constructor(){this.builtInTools=[c.Undo,c.Size,c.List];this.isE2E=x(g);this.control=new T(`
+    <p>Which do you prefer?</p>
+    <ul data-type="taskList">
+        <li
+            data-checked="true"
+            data-type="taskItem"
+        >
+            <label>
+                <input
+                    checked="checked"
+                    type="checkbox"
+                />
+                <span></span>
+            </label>
+            <div>
+                <p>Taiga UI</p>
+                <ul data-type="taskList">
+                    <li
+                        data-checked="false"
+                        data-type="taskItem"
+                    >
+                        <label>
+                            <input type="checkbox" />
+                            <span></span>
+                        </label>
+                        <div><p>v3.x</p></div>
+                    </li>
+                    <li
+                        data-checked="true"
+                        data-type="taskItem"
+                    >
+                        <label>
+                            <input
+                                checked="checked"
+                                type="checkbox"
+                            />
+                            <span></span>
+                        </label>
+                        <div><p>v4.x</p></div>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li
+            data-checked="false"
+            data-type="taskItem"
+        >
+            <label>
+                <input type="checkbox" />
+                <span></span>
+            </label>
+            <div><p>Material</p></div>
+        </li>
+        <li
+            data-checked="false"
+            data-type="taskItem"
+        >
+            <label>
+                <input type="checkbox" />
+                <span></span>
+            </label>
+            <div><p>Prime NG</p></div>
+        </li>
+        <li
+            data-checked="false"
+            data-type="taskItem"
+        >
+            <label>
+                <input type="checkbox" />
+                <span></span>
+            </label>
+            <div><p>NG-Zorro</p></div>
+        </li>
+        <li
+            data-checked="false"
+            data-type="taskItem"
+        >
+            <label>
+                <input type="checkbox" />
+                <span></span>
+            </label>
+            <div><p>Bootstrap</p></div>
+        </li>
+    </ul>
+    <p></p>
+    `)}};n.\u0275fac=function(e){return new(e||n)},n.\u0275cmp=b({type:n,selectors:[["ng-component"]],features:[h([N()])],decls:10,vars:5,consts:[[3,"formControl","tools"],[3,"closeOthers"],[3,"tuiAccordion"],[4,"tuiItem"],[3,"content"],[1,"code"]],template:function(e,l){e&1&&(m(0,"tui-editor",0),a(1,"tui-accordion",1)(2,"button",2),r(3,"HTML"),o(),a(4,"tui-expand"),s(5,L,2,1,"ng-container",3),o(),a(6,"button",2),r(7,"Text"),o(),a(8,"tui-expand"),s(9,P,3,1,"ng-container",3),o()()),e&2&&(i("formControl",l.control)("tools",l.builtInTools),t(),i("closeOthers",!1),t(),i("tuiAccordion",l.isE2E),t(4),i("tuiAccordion",l.isE2E))},dependencies:[I,E,C,D,A,_,S,M,F],encapsulation:2,changeDetection:0});var k=n;export{k as default};
