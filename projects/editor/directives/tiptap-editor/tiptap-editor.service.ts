b9519e4d3da926fda1890385f113d3e9cf4237f5
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-08-10 12:36:30 +0300

    fix(editor): make compatible with `@tiptap/extension-youtube@3.28.0` (#2249)

--- projects/editor/directives/tiptap-editor/tiptap-editor.service.ts
+++ projects/editor/directives/tiptap-editor/tiptap-editor.service.ts
@@ -446,12 +446,7 @@ export class TuiTiptapEditorService extends AbstractTuiEditor {
     }
 
     public setYoutubeVideo(options: TuiYoutubeOptions): void {
-        // TipTap restricts width/height to number, but the iframe attribute accepts strings like '100%'
-        this.editor?.commands.setYoutubeVideo(
-            options as unknown as Parameters<
-                typeof this.editor.commands.setYoutubeVideo
-            >[0],
-        );
+        this.editor?.commands.setYoutubeVideo(options);
     }
 
     public setIframe(options: TuiEditableIframe): void {
