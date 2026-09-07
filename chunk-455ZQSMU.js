ee5e0069e298ba2cea9b2f1cc3251b94bc8f5d7a
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2026-09-07 06:44:52 +0000

    Deploying to gh-pages from @ taiga-family/editor@d76daece8edc4f386abdacd1ab7e7c2a1666d4f4 🚀

---
+++ chunk-455ZQSMU.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-CA6JALRB.js";import"./chunk-UFFE5ZMZ.js";import"./chunk-PB4PSOPL.js";import"./chunk-BFTB7GM2.js";import"./chunk-HSTYGBAB.js";import"./chunk-HFB2H5VX.js";import"./chunk-MV4OHMNG.js";import"./chunk-C6DAHHHE.js";import"./chunk-DAQOROHW.js";export{a as TuiDetails,c as TuiDetailsContent,b as TuiDetailsSummary};
